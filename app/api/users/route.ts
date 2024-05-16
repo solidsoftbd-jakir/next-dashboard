6eeede865b2c94ee274cb990f1f4f086b66693ab
jakir-datavanced <jakirdatavanced22@gmail.com>
2024-05-16 12:53:33 +0600

    added route for postgres

---
+++ app/api/users/route.ts
@@ -0,0 +1,8 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+    const users = await sql`SELECT * FROM users;`;
+    return NextResponse.json(users);
+  
+}
