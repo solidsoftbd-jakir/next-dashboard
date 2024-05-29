b7465f14ed3fd3d8b471a37262f459cfbb4630a2
jakir-datavanced <jakirdatavanced22@gmail.com>
2024-05-29 12:32:31 +0600

    Add readl data

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,16 @@
-import { sql } from '@vercel/postgres';
+import { db, sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET(request: Request) {
-    const users = await sql`SELECT * FROM users;`;
-    return NextResponse.json(users);
-  
+    const client = await db.connect();
+
+    try {
+        const result = await client.query('SELECT * FROM products;');
+        return NextResponse.json(result.rows); // Return only the rows
+    } catch (error) {
+        console.error('Error querying products:', error);
+        return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
+    } finally {
+        client.release();
+    }
 }
